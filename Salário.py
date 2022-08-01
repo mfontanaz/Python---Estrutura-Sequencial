6a28edb8d0b40059ab73b418ae6b0b450582a13d
Murilo Fontana <110347395+mfontanaz@users.noreply.github.com>
2022-08-01 10:20:28 -0300

    Add files via upload

---
+++ Salário.py
@@ -0,0 +1,5 @@
+
+ganho_hora = float(input("Quanto ganha por hora trabalhada: "))
+hora_mes = float(input("Quantas horas você trabalha por mês: "))
+salario_mes = ganho_hora*hora_mes
+print(f"Seu salário este mes será R$: ", salario_mes)
