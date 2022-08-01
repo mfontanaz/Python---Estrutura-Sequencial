514470f6bf5d7c8047b736c7dcab1204266dedea
Murilo Fontana <110347395+mfontanaz@users.noreply.github.com>
2022-08-01 08:59:01 -0300

    Add files via upload

---
+++ area_quadrado.py
@@ -0,0 +1,5 @@
+print("Programa que calcula a área de um quadrado, e mostra o dobro desta área.")
+altura = float(input("Digite a altura do quadrado em metros: "))
+largura = float(input("Digite a largura do quadrado em metros: "))
+area = altura*largura
+print ("O valor do dobro da área do quadrado é: ", 2*area)
